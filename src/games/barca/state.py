84d0bebd785d2aac183c97b72aa3896f38042d13
extremelf <luis.paulo.6.1.2001@gmail.com>
2022-05-05 11:55:28 +0100

    Board display

--- src/games/barca/state.py
+++ src/games/barca/state.py
@@ -19,7 +19,7 @@ class BarcaState(State):
         self.__num_rows = num_rows
         self.__num_cols = num_cols
 
-        self.__grid = [BarcaState.EMPTY_CELL for _i in range(self.__num_cols) for _j in range(self.__num_rows)]
+        self.__grid = [[BarcaState.EMPTY_CELL for _i in range(self.__num_cols)] for _j in range(self.__num_rows)]
 
         self.__turns_count = 1
 
@@ -56,25 +56,58 @@ class BarcaState(State):
             for piece in player.pieces:
                 current_pos = piece.get_current_pos()
                 if isinstance(piece, Elephant):
-                    print(piece.possible_moves())
-                    grid[current_pos[1]*10+current_pos[0]] = 2
+                    grid[current_pos[1]][current_pos[0]-1] = 2
                 elif isinstance(piece, Lion):
-                    grid[current_pos[1]*10+current_pos[0]] = 3
+                    grid[current_pos[1]][current_pos[0]-1] = 3
                 elif isinstance(piece, Mouse):
-                    grid[current_pos[1]*10 +current_pos[0]] = 4
+                    grid[current_pos[1]][current_pos[0]-1] = 4
 
         self.display_grid(grid)
+
+    def display_grid(self, grid):
+
+        self.__displayer_separator()
+        self.__displayer_numbers()
+
+        for row in range(0, self.__num_rows):
+            print(row, end="")
+            print("|", end="")
+            for col in range(0, self.__num_cols):
+                self.__displayer_cell(row, col)
+                print("|", end="")
+            print("")
+            self.__displayer_separator()
+        self.__displayer_numbers()
+        print("")
+        #for pos in range(len(grid)):
+         #   if pos % 10 == 9:
+          #      print(f' {grid[pos]}', end="\n")
+           # else:
+            #    print(f' {grid[pos]}', end=" ")
+
+    def __displayer_cell(self, row, col):
+        print({
+                  2: ' E ',
+                  3: ' L ',
+                  4: ' M ',
+                  BarcaState.EMPTY_CELL: '   '
+              }[self.__grid[row][col]], end="")
+
+    def __displayer_separator(self):
+        for col in range(0, self.__num_cols):
+            print("----", end="")
+        print("---")
+
+    def __displayer_numbers(self):
+        for col in range(0, self.__num_cols):
+            if col < 10:
+                print(' ', end="")
+            print(f' {col} ', end="")
+        print("")
 
     def get_opponent_cords(self, player):
         opponent_pieces_coords = player.pieces.get_current_position
 
-
-    def display_grid(self, grid):
-        for pos in range(len(grid)):
-            if pos % 10 == 9:
-                print(f' {grid[pos]}', end="\n")
-            else:
-                print(f' {grid[pos]}', end=" ")
     def get_legal_moves(self, piece, opponent_pieces_coords):
 
         all_moves = piece.possible_moves
@@ -92,10 +125,12 @@ class BarcaState(State):
 
     def get_num_players(self):
         pass
+
     def get_result(self, pos):
         pass
+
     def is_finished(self) -> bool:
         pass
+
     def validate_action(self, action) -> bool:
         pass
-
